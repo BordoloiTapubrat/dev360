771440ba552366e2d2c4b03c1b6105672a9b244e
TAPUBRAT\tapub <tapubrat.bordoloi@gmail.com>
2025-03-28 14:53:00 +0530

    social media links

---
+++ js/socialMedia.js
@@ -0,0 +1,6 @@
+var navBar =
+  '<div class = "icon-bar">\
+  <a href="http" class="meta">Meta</a>\
+<a href="http" class="twitter">X</a>\
+  <a href="http" class="instagram">Instagram</a></div>';
+document.getElementById("socialMedia").innerHTML = navBar;
